6631f95d15e017e90ba8c64e2d838fd439e50f9f
Diego García <dgarciaortiz94@gmail.com>
2021-11-14 12:59:48 +0100

    deleted fields of VichUploaderBundle from pocast entity

--- src/Entity/Podcast.php
+++ src/Entity/Podcast.php
@@ -3,12 +3,10 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use App\Repository\PodcastRepository;
 
 /**
- * @ORM\Entity(repositoryClass=PodcastRepository::class)* 
- * @Vich\Uploadable
+ * @ORM\Entity(repositoryClass=PodcastRepository::class)
  */
 class Podcast
 {
@@ -30,46 +28,20 @@ class Podcast
     private $description;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string")
+     *
+     * @var string|null
      */
     private $audio;
 
     /**
-     * NOTE: This is not a mapped field of entity metadata, just a simple property.
-     * 
-     * @Vich\UploadableField(mapping="podcast_audio", fileNameProperty="audio", size="10485760")
-     * 
-     * @var File|null
-     */
-    private $audioFile;
-
-    /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="string")
      *
-     * @var int|null
-     */
-    private $audioSize;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
      */
     private $picture;
 
-    /**
-     * NOTE: This is not a mapped field of entity metadata, just a simple property.
-     * 
-     * @Vich\UploadableField(mapping="podcast_picture", fileNameProperty="picture", size="10485760")
-     * 
-     * @var File|null
-     */
-    private $pictureFile;
 
-    /**
-     * @ORM\Column(type="integer")
-     *
-     * @var int|null
-     */
-    private $pictureSize;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="podcasts")
@@ -82,19 +54,6 @@ class Podcast
      */
     private $createDate;
 
-    /**
-     * @ORM\Column(type="datetime")
-     *
-     * @var \DateTimeInterface|null
-     */
-    private $updatedAudio;
-
-    /**
-     * @ORM\Column(type="datetime")
-     *
-     * @var \DateTimeInterface|null
-     */
-    private $updatedPicture;
 
     public function getId(): ?int
     {
@@ -125,30 +84,6 @@ class Podcast
         return $this;
     }
 
-    // public function getAudio(): ?string
-    // {
-    //     return $this->audio;
-    // }
-
-    // public function setAudio(?string $audio): self
-    // {
-    //     $this->audio = $audio;
-
-    //     return $this;
-    // }
-
-    // public function getPicture(): ?string
-    // {
-    //     return $this->picture;
-    // }
-
-    // public function setPicture(?string $picture): self
-    // {
-    //     $this->picture = $picture;
-
-    //     return $this;
-    // }
-
     public function getUser(): ?User
     {
         return $this->user;
@@ -173,30 +108,6 @@ class Podcast
         return $this;
     }
 
-    
-
-    /**
-     * 
-     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $audioFile
-     */
-    public function setAudioFile(?File $audioFile = null): void
-    {
-        $this->audioFile = $audioFile;
-
-        if (null !== $audioFile) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAudio = new \DateTimeImmutable();
-        }
-    }
-
-    public function getAudioFile(): ?File
-    {
-        return $this->audioFile;
-    }
-
-
-
     public function setAudio(?string $audio): void
     {
         $this->audio = $audio;
@@ -206,42 +117,6 @@ class Podcast
     {
         return $this->audio;
     }
-
-
-    
-    public function setAudioSize(?int $audioSize): void
-    {
-        $this->audioSize = $audioSize;
-    }
-
-    public function getAudioSize(): ?int
-    {
-        return $this->audioSize;
-    }
-
-
-
-    /**
-     * 
-     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $pictureFile
-     */
-    public function setPictureFile(?File $pictureFile = null): void
-    {
-        $this->pictureFile = $pictureFile;
-
-        if (null !== $pictureFile) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->updatedPicture = new \DateTimeImmutable();
-        }
-    }
-
-    public function getPictureFile(): ?File
-    {
-        return $this->pictureFile;
-    }
-
-
 
     public function setPicture(?string $picture): void
     {
@@ -253,15 +128,4 @@ class Podcast
         return $this->picture;
     }
 
-
-    
-    public function setPictureSize(?int $pictureSize): void
-    {
-        $this->pictureSize = $pictureSize;
-    }
-
-    public function getPictureSize(): ?int
-    {
-        return $this->pictureSize;
-    }
 }
