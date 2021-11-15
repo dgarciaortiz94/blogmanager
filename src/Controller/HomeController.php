fa893691d874942658dbb6c6436927309bbf897c
Diego García <dgarciaortiz94@gmail.com>
2021-11-15 19:56:29 +0100

    home controller created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Podcast;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,11 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $podcastRepository = $this->getDoctrine()->getRepository(Podcast::class);
+        $treeLastPodcasts = $podcastRepository->getLastPodcasts(3);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'podcasts' => $treeLastPodcasts
         ]);
     }
 }
