3b2de9a1e4fc5e14a1a567cd42c5f1232f98db4e
Diego García <dgarciaortiz94@gmail.com>
2021-11-15 13:37:25 +0100

    display podcasts-working

---
+++ src/Controller/DisplaypodcastController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Podcast;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DisplaypodcastController extends AbstractController
+{
+    /**
+     * @Route("/ver-podcast/{podcastName}", name="displaypodcast")
+     */
+    public function index($podcastName): Response
+    {
+        $podcastName = str_replace("-", " ",$podcastName);
+        
+        $podcastRepository = $this->getDoctrine()->getRepository(Podcast::class);
+
+        $podcast = $podcastRepository->findOneBy(array('title' => $podcastName));
+
+
+
+
+        return $this->render('displaypodcast/index.html.twig', [
+            'podcast' => $podcast
+        ]);
+    }
+}
