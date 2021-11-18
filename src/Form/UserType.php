efefdff868ff588fc0b54640440b1192d598beaf
Diego García <dgarciaortiz94@gmail.com>
2021-11-18 11:46:32 +0100

    bug edit users fixed

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +16,25 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => false
+            ])
             ->add('roles', ChoiceType::class, [
+                'label' => false,
                 'choices'  => [
                     'ROLE_ADMIN' => '[ROLE_ADMIN]',
                     'ROLE_USER' => '[ROLE_USER]',
                 ],
             ])
-            ->add('firstname')
-            ->add('lastname')
-            ->add('name')
+            ->add('firstname', TextType::class, [
+                'label' => false
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => false
+            ])
+            ->add('name', TextType::class, [
+                'label' => false
+            ])
         ;
 
         //roles field data transformer
